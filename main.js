432c0e520ac7a26f00fb315efd06210f05eb1e4f
magicoutside <37013164+magicoutside@users.noreply.github.com>
2018-03-27 10:08:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -28,3 +28,47 @@ for(let i=0; i < salaries.length; i++){
      }
  }
  console.log(min)
+
+// filter
+var filter = (arr, condition) => {
+	var result = [];
+	for (let i = 0; i < arr.length; i++) {
+	
+		if (condition(arr[i])) {
+			result.push(arr[i]);
+		}
+	}
+	return result;
+}; 
+
+filter([1,2,3,4,5,6], (e) => e > 4)
+// find
+var find = (arr, element) => {
+	var found = filter(arr, (e) => e == element);
+	return found.length == 1 
+		? found[0] 
+		: null;
+} 
+// map
+var map = (arr, transformation) => {
+	var result= [];
+	for (let i = 0; i < arr.length; i++) {
+        result.push(transformation(arr[i]));
+	}
+	return result;
+};
+// reduce
+let reduce = (arr, reducer, initialValue) => {
+	
+	let result = initialValue;
+
+	for (let i = 0; i < arr.length; i++) {
+	
+		result = reducer(arr[i], result);
+
+	}
+
+	return result;
+
+}
+reduce([1,2,3,4], (el, acc) => el + acc, 0);
