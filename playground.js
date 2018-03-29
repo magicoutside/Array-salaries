3f61edfc80e4e94d07a9675df66dd66d95f203df
Студент <ab2@EU.NET>
2018-03-29 10:08:59 +0300

    add git commands

---
+++ playground.js
@@ -0,0 +1,7 @@
+/*\
+cd - change dir
+ls - list directory content
+git clone - clone repository
+cat -  provide content or type and size information for repository objects
+rm - delete something
+*/
