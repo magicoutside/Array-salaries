dbfc885d3ae036320f73d1d5b832565f1499bca1
unknown <@309-11.EU.NET>
2018-04-05 09:28:08 +0300

    playground

--- playground.js
+++ playground.js
@@ -5,3 +5,16 @@ git clone - clone repository
 cat -  provide content or type and size information for repository objects
 rm - delete something
 */
+var assertEquals = (expected, actual) => {
+    if (expected != actual)
+        throw `Expected ${expected}, but got ${actual}`;
+};
+
+var person = {
+    name: 'Ivan',
+    age: 13
+};
+
+assertEquals(person.name, 'Ivan');
+assertEquals(person.age , 13);
+console.log(person)
