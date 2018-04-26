7c371acd59270502f26643a83cc84d48740d4430
unknown <@309-11.EU.NET>
2018-04-26 09:53:47 +0300

    text

--- text.js
+++ text.js
@@ -11,7 +11,11 @@ var assertArraysEquals = (ar1, ar2) => {
 }
 
 let text = "some text is written here";
-let getWords = (txt) => {
-    //...
+let getWords = (str) => {
+    let words = str.split(" ");
+for (let i = 0; i < words.length - 1; i++) {
+    words[i] += " ";
+}
+console.log(words);
 };
 assertArraysEquals(getWords(text), ["some","text","is", "written", "here"])
