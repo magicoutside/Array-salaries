0bb37f9d8145867adde6e942c41371bf91f6ad42
unknown <@309-11.EU.NET>
2018-04-26 10:22:24 +0300

    asd

--- text.js
+++ text.js
@@ -1,3 +1,4 @@
+//Shift + Alt + F -- format code
 var assertEquals = (expected, actual) => {
     if (expected != actual)
         throw `Expected ${expected}, but got ${actual}`;
@@ -5,17 +6,23 @@ var assertEquals = (expected, actual) => {
 
 var assertArraysEquals = (ar1, ar2) => {
     assertEquals(ar1.length, ar2.length);
-	for (i = 0; i < ar1.length; i++) { 
-		assertEquals(ar1[i], ar2[i]);
+    for (i = 0; i < ar1.length; i++) {
+        assertEquals(ar1[i], ar2[i]);
     }
 }
 
 let text = "some text is written here";
 let getWords = (str) => {
     let words = str.split(" ");
-for (let i = 0; i < words.length - 1; i++) {
-    words[i] += " ";
-}
-console.log(words);
+    for (let i = 0; i < words.length - 1; i++) {
+        words[i] += " ";
+        console.log(words[i]);
+    }
+    // console.log(words);
 };
-assertArraysEquals(getWords(text), ["some","text","is", "written", "here"])
+
+
+console.log(getWords(text));
+
+
+// assertArraysEquals(getWords(text), ["some", "text", "is", "written", "here"]);
