c5979ebbfb78b01fd0d1098faef54f36640fafae
Студент <ab2@EU.NET>
2018-06-16 13:14:04 +0300

    quicksort

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 let salaries = [1700, 2000, 1560, 2300, 1920, 1400]
 let y = salaries.reduce(function (sum, el){
 	return sum + el;
@@ -104,3 +105,32 @@ for (var property1 in assertObjectsEquals) {
 	string = string + assertObjectsEquals[property1];
 }
 console.log(string)
+*/
+const filter = (arr, condition) => {
+	var result = [];
+	for (let i = 0; i < arr.length; i++) {
+		
+		if (condition(arr[i])) {
+			result.push(arr[i]);
+		}
+	}
+	return result;
+}; 
+
+
+
+const last = arr => arr[arr.length - 1];
+const quickSort = (arr) => {
+	if (arr.length <= 1) return arr;
+
+	const center = last(arr);
+	const left = arr.filter(el => el < center);
+	const right = arr.filter((el, i) => el >= center && i !== arr.length - 1);
+
+	return [...quickSort(left), center, ...quickSort(right)];
+}
+
+const testArr = [6, 7, 4, 1, 2, 0, 3, 3, 3, 5, 5, 10, 10];
+
+const res = quickSort(testArr);
+console.log(res);
